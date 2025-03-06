095ccf342c8128c89f205146cbc0270746c73794
CrisEvaristo <cristovaoevaristomanuel@gmail.com>
2025-03-06 15:25:43 +0100

    adicionando a estrutura de pastas

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+
 import server from "./server/server";
 
-server.listen(3333, () => console.log('Servidor rodando na porta 3333'));
+
+server.listen(process.env.PORT||3333, () => {
+  console.log('Servidor rodando na porta 3333')
+});
