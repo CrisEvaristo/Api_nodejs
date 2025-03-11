0b8c8b61fc7cca430ce3468518a334b0c932a8ab
Cristóvão Evaristo <cristovaoevaristomanuel@gmail.com>
2025-03-11 17:10:59 +0100

    implementação do banco de dados e dos testes

--- build/server/routes/index.js
+++ build/server/routes/index.js
@@ -1,15 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const controllers_1 = require("./../controllers");
 const router = (0, express_1.Router)();
-router.get('/teste', (req, res) => {
-    return res.send('ola dev');
-});
 router.get('/', (req, res) => {
-    return res.send('ola principal');
+    return res.send('Bem-vindo');
 });
-router.post('/teste', (req, res) => {
-    console.log(req.body);
-    return res.send('testando psot');
-});
+router.get('/cidades', controllers_1.CidadeController.getAllValidation, controllers_1.CidadeController.getAll);
+router.get('/cidades/:id', controllers_1.CidadeController.getByIdValidation, controllers_1.CidadeController.getById);
+router.post('/cidades', controllers_1.CidadeController.createValidation, controllers_1.CidadeController.create);
+router.put('/cidades/:id', controllers_1.CidadeController.updateByIdValidation, controllers_1.CidadeController.updateById);
+router.get('/cidades/:id', controllers_1.CidadeController.deleteByIdValidation, controllers_1.CidadeController.deleteById);
 exports.default = router;
